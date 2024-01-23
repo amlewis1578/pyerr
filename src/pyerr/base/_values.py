4dfe352c2dd741cb815ecb6fc38eab71b6a6658e
amlewis1578 <amlewis1578@gmail.com>
2024-01-23 10:46:13 -0700

    adding base values parsing class

---
+++ src/pyerr/base/_values.py
@@ -0,0 +1,46 @@
+import fortranformat as ff
+from abc import ABC
+
+class Values(ABC):
+    """
+    Abstract class to parse value lines from the ERRORR files. 
+
+    Parameters
+    ----------
+    lines : list
+        list of control lines from the file
+
+    Attributes
+    ----------
+    lines : list
+        list of control lines from the file
+
+    parsed_values : list
+        list of the values in the section
+
+    Methods
+    -------
+    parse_lines
+        Parse the lines by their format string
+
+
+    """
+    def __init__(self,lines):
+        self.lines = lines
+        self.parse_lines()
+
+    def parse_lines(self):
+        """ Parse the values lines by their format string 
+        
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None, sets the attribute self.parsed_values
+
+        """
+        control_line = ff.FortranRecordReader('(6G11.0)')
+        parsed_lines = [control_line.read(line) for line in self.lines]
+        self.parsed_values = [item for line in parsed_lines for item in line]
