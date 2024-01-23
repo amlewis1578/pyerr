ef23c5d4149dd427f6c1d6981a4828a139204667
amlewis1578 <amlewis1578@gmail.com>
2024-01-23 14:22:38 -0700

    updating version

--- docs/versions.md
+++ docs/versions.md
@@ -1,3 +1,4 @@
 - **0**
     - `0.1.0` - Energy group classes
     - `0.2.0` - Mean value classes
+    - `0.3.0` - Covariance classes
--- src/pyerr/__init__.py
+++ src/pyerr/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from pyerr._energy import EnergyGroupControl, EnergyGroupValues, EnergyGroups
 from pyerr._mean import MeanControl, MeanValues, Mean
