19b2d78ddf6b63ffd4d4005a486c3c5564ba78ac
amlewis1578 <amlewis1578@gmail.com>
2024-07-16 12:11:50 -0600

    fix value in convergence test

--- tests/convergence_test.py
+++ tests/convergence_test.py
@@ -13,9 +13,9 @@ def test_covergence_table_limit_low(u235_endf81):
     obj = ErrorrOutput(u235_endf81)
     pfns = obj.sections[18]
     pfns.quantify_uncertainty_convergence(e_min=11)
-    print(pfns.unc_convergence_table.head())
     assert 'k' in pfns.unc_convergence_table.columns
-    assert pfns.unc_convergence_table.rel_diff.iloc[-1] < 1e-4
+    print(pfns.unc_convergence_table.tail())
+    assert pfns.unc_convergence_table.rel_diff.iloc[-1] < 2e-4
     assert pfns.unc_convergence_table.rel_diff.iloc[0] > 0.99
     assert pfns.unc_convergence_table.rel_ind.iloc[0] == 597
 
